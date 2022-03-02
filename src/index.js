85740a3bfc91f8553bd958e4255c468985e69058
xia.huang <xia.huang@convertlab.com>
2022-03-02 15:31:32 +0800

    配置antd按需引入：安装和配置babel-plugin-import插件

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDom from "react-dom";
 import App from "./App";
 import "../static/css/reset.min.css";
+// babel-plugin-import插件会在引入antd组件的时候自动按需引入需要的样式
 // import "antd/dist/antd.css";
 
 ReactDom.render(<App />, document.getElementById("app"));
